576fd954dbf424f823e34aa645f79448e37b83e4
worada <worada.wa@ku.th>
2021-01-04 17:43:14 +0700

    Reformat Code

--- src/CheckingPrimeNumber.kt
+++ src/CheckingPrimeNumber.kt
@@ -1,10 +1,10 @@
-class CheckingPrimeNumber(num: Int) : PrimeNumberSet(num), PrimeNumbers, Outcome{
+class CheckingPrimeNumber(num: Int) : PrimeNumberSet(num), PrimeNumbers, Outcome {
 
-    override fun showOutcome(){
+    override fun showOutcome() {
         println("$num : is prime number")
     }
 
-    var primeNumbers : Boolean = true
+    var primeNumbers: Boolean = true
     override fun isPrimeNumbers(): Boolean {
         return primeNumbers
     }
