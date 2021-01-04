cd21e57063245ed9d4d6514cd8315f2a9f7846e4
Vittee Nakka <vittee@hotmail.com>
2021-01-04 18:19:19 +0700

    Make num property immutable.

--- src/MaybePrimeInt.kt
+++ src/MaybePrimeInt.kt
@@ -1,4 +1,4 @@
-class MaybePrimeInt(var num: Int) : MaybePrimeNumber {
+class MaybePrimeInt(val num: Int) : MaybePrimeNumber {
 
     override fun isPrimeNumber(): Boolean {
         TODO("Implement prime number checking logic here")
