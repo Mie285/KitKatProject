b2982ac239539a5b47f54f05e37f183e636f26f4
worada <worada.wa@ku.th>
2021-01-04 17:59:19 +0700

    change variable name

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val setNumbers = listOf(
+    val setOfNumbers = listOf(
         ShowingValue(2), ShowingValue(3), ShowingValue(5),
         ShowingValue(7), ShowingValue(11), ShowingValue(13),
         ShowingValue(17), ShowingValue(19), ShowingValue(23),
@@ -10,8 +10,8 @@ fun main() {
         ShowingValue(79), ShowingValue(83), ShowingValue(89),
         ShowingValue(97)
     )
-    
-    val num = setNumbers.filter { it.isPrimeNumbers() }
+
+    val num = setOfNumbers.filter { it.isPrimeNumbers() }
     for (set in num) {
         set.showOutcome()
     }
