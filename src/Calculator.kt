4513d139bb2e9724ea3854e492111054d0c0c35e
worada <worada.wa@ku.th>
2021-01-04 17:43:18 +0700

    Reformat Code

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,18 +1,18 @@
 fun main() {
 
     val setNumbers = listOf(
-            ShowingValue(2), ShowingValue(3), ShowingValue(5),
-            ShowingValue(7), ShowingValue(11), ShowingValue(13),
-            ShowingValue(17), ShowingValue(19), ShowingValue(23),
-            ShowingValue(29), ShowingValue(31), ShowingValue(37),
-            ShowingValue(41), ShowingValue(59), ShowingValue(61),
-            ShowingValue(67), ShowingValue(71), ShowingValue(73),
-            ShowingValue(79), ShowingValue(83), ShowingValue(89),
-            ShowingValue(97)
+        ShowingValue(2), ShowingValue(3), ShowingValue(5),
+        ShowingValue(7), ShowingValue(11), ShowingValue(13),
+        ShowingValue(17), ShowingValue(19), ShowingValue(23),
+        ShowingValue(29), ShowingValue(31), ShowingValue(37),
+        ShowingValue(41), ShowingValue(59), ShowingValue(61),
+        ShowingValue(67), ShowingValue(71), ShowingValue(73),
+        ShowingValue(79), ShowingValue(83), ShowingValue(89),
+        ShowingValue(97)
     )
     
     val num = setNumbers.filter { it.isPrimeNumbers() }
-    for (set in num){
+    for (set in num) {
         set.showOutcome()
     }
 }
