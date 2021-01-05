29fe96ceb61890d287eb5230edad25a84252045c
worada <worada.wa@ku.th>
2021-01-05 17:47:44 +0700

    reformat

--- src/FindingPrimeNumber.kt
+++ src/FindingPrimeNumber.kt
@@ -1,14 +1,8 @@
 fun main() {
-<<<<<<< HEAD
+
     val numbers = (1..100).map(::MaybePrimeInt)
     val primeNumbers = numbers.filter { it.isPrimeNumber() }
     //val primeNumbers = numbers.filter(MaybePrimeInt::isPrimeNumber)
     print(primeNumbers)
-=======
 
-    val numbers = (1..10).map(::MaybePrimeInt)
-    val primeNumbers = numbers.filter { it.isPrimeNumber() }
-    print(primeNumbers)
-
->>>>>>> f578be90ef7c1e0063c8a51ff71452d457d448a8
 }
