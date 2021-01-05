7645023d87a694f2d8a807965e4f2c759114ec3e
tanaporn.suea <tanaporn.sues@ku.th>
2021-01-05 10:19:16 +0700

    check prime number

---
+++ src/MaybePrimeInt.kt
@@ -0,0 +1,16 @@
+class MaybePrimeInt(val value: Int) : MaybePrimeNumber {
+
+    override fun toString(): String {
+        return value.toString()
+    }
+
+    override fun isPrimeNumber(): Boolean {
+        if (value < 2) return false
+        for (number in 2..value/2) {
+            if (value % number == 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
