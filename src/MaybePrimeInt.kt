55eaa5b65b50d5e4c470979fcab398fac506ab85
tanaporn.suea <tanaporn.sues@ku.th>
2021-01-05 16:07:44 +0700

    reformat

--- src/MaybePrimeInt.kt
+++ src/MaybePrimeInt.kt
@@ -6,7 +6,7 @@ class MaybePrimeInt(val value: Int) : MaybePrimeNumber {
 
     override fun isPrimeNumber(): Boolean {
         if (value < 2) return false
-        for (number in 2..value/2) {
+        for (number in 2..value / 2) {
             if (value % number == 0) {
                 return false
             }
