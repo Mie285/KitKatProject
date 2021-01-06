640f06a8ad4841c9ca157dccd2c26882f4b62577
Vittee Nakka <vittee@hotmail.com>
2021-01-06 11:41:46 +0700

    Use lambda instead of function reference.

--- src/FindingPrimeNumber.kt
+++ src/FindingPrimeNumber.kt
@@ -1,6 +1,6 @@
 fun main() {
 
-    val numbers = (1..100).map(::MaybePrimeInt)
+    val numbers = (1..100).map { MaybePrimeInt(it) }
     val primeNumbers = numbers.filter { it.isPrimeNumber() }
     print(primeNumbers)
 }
