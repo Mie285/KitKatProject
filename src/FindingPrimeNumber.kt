ff9316822f9e0b1c133c9330b691c7aa4bdd6aeb
tanaporn.suea <tanaporn.sues@ku.th>
2021-01-06 11:31:17 +0700

    delete comment

--- src/FindingPrimeNumber.kt
+++ src/FindingPrimeNumber.kt
@@ -2,7 +2,5 @@ fun main() {
 
     val numbers = (1..100).map(::MaybePrimeInt)
     val primeNumbers = numbers.filter { it.isPrimeNumber() }
-    //val primeNumbers = numbers.filter(MaybePrimeInt::isPrimeNumber)
     print(primeNumbers)
-
 }
